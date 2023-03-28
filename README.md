8648104ae0ee5119e749da39746cc93d1a097c58
Aditya <itsadityasharma7124@gmail.com>
2023-03-29 00:18:53 +0530

    styles updated

---
+++ README.md
@@ -0,0 +1,16 @@
+# React to-do list
+
+Steps to run this project
+
+## Clone:
+git clone https://github.com/its-me-aadi/React-to-dolist.git
+
+## Go to project folder
+cd to-dolist
+
+### Run `npm start` 
+
+Runs the app in the development mode.\
+Open [http://localhost:3000](http://localhost:3000) to view it in your browser.
+
+The page will reload when you make changes.
---
+++ src/components/App.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+
+
+function App() {
+  const [note,setNote]=useState([{title:"Shree ram",content:"Janki"}]);
+  const [newNote,setNewNote]=useState({
+    title:"",
+    content:""
+  })
+
+  function createNote(event){
+    const {value,name}=event.target;
+    setNewNote(prevValue=>{
+      return {
+        ...prevValue,
+        [name]:value
+      }
+    });
+    console.log(newNote);
+  }
+
+  function AddNote(event){
+    console.log(note);
+    setNote(prevValue=>[...prevValue,{
+      title:newNote.title,
+      content:newNote.content
+    }])
+    console.log(note);
+    console.log("clicked");
+    event.preventDefault();
+  }
+
+  function DeleteNote(id){
+    console.log(id);
+    setNote(prevValue=>{
+      return (
+        prevValue.filter((value,index)=>{
+          return index!==id;
+        })
+      )
+    })
+  }
+
+  return (
+    <div>
+      <Header />
+      <CreateArea onChange={createNote} onClick={AddNote} title={newNote.title} value={newNote.content}/>
+      {note.map((item,index)=>{
+        return (
+          <Note key={index} id={index} title={item.title} content={item.content} onClick={DeleteNote}/>
+        )  
+      })}
+      
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
